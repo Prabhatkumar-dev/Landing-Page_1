5dc1dc7895cd6ff0818ee7d57e2331c5140363a8
Prabhat Kumar <prabhat.dev0108@gmail.com>
2025-09-02 11:35:43 +0530

    added first time

---
+++ one/script.js
@@ -0,0 +1,79 @@
+// import LocomotiveScroll from 'locomotive-scroll';
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector("#main"),
+    smooth: true
+});
+
+// let main = document.querySelector("#main");
+// let cursor = document.querySelector("#minicircle");
+// window.addEventListener("mousemove",function(dets){
+//     gsap.to(cursor,{
+//         x: dets.x ,
+//         y: dets.y ,
+//         duration : 0.2
+//     })
+// })
+
+function firstPageAnimation(){
+    var tl =gsap.timeline();
+    tl.from("#nav",{
+        y:"-10",
+        opacity:0,
+        duration:1.5,
+        ease : Expo.easeInOut
+        
+    })
+
+   
+    tl.to(".boundingelem",{
+        y:0,
+       delay:-1,
+        duration:1.5,
+        ease : Expo.easeInOut,
+        stagger :.2
+        
+    })
+    tl.from("#herofooter",{
+        y:-10,
+        opacity:0,
+        ease:Expo.easeInOut,
+        duration :1.5,
+        delay :-1.2
+    })
+}
+  function circleMouseFollower(){
+     window.addEventListener("mousemove", function(dets){
+      document.querySelector("#minicircle").style.transform = `translate(${dets.clientX}px,${dets.clientY}px)`;
+      })
+    }
+       circleMouseFollower();
+       firstPageAnimation();
+
+       //ab hum wo image wla bnayange , teeno elem ko select krenge aur teeni pe mouse move wla lagayenge, ab mouse move hone pr uski details chahiye hogi  
+ document.querySelectorAll(".elem").forEach(function(elem){
+    var rotate = 0;
+    var diffrot =0;
+    
+    elem.addEventListener("mouseleave", function(dets){
+       gsap.to(elem.querySelector("img"),{
+        opacity : 0, 
+        ease : Power3 ,
+        duration:1,
+        
+       });
+    });
+
+     elem.addEventListener("mousemove", function(dets){
+        let diff = dets.clientY - elem.getBoundingClientRect().top;
+        diffrot = dets.clientX - rotate;
+        rotate = dets.clientX;
+       gsap.to(elem.querySelector("img"),{
+        opacity : 1, 
+        ease : Power3 ,
+        top : diff  ,
+        left : dets.clientX,
+        rotate :gsap.utils.clamp(-20 ,20 , diffrot *0.8),
+       });
+    });
+ });
